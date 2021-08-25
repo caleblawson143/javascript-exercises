2c909d3fa91bac64e75ba1501e2ee3ef8230bdbd
caleblawson143 <56615142+caleblawson143@users.noreply.github.com>
2021-08-25 12:53:24 -0500

    Add files via upload

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const ftoc = function() {
+let cTempRounded = 0;
+let fTempRounded = 0;
 
+const ftoc = function(fTemp) {
+  let ftocTemp = (fTemp - 32) * .556;
+  if (ftocTemp >= 0) {
+    cTempRounded = Math.round(ftocTemp * 10) / 10;
+  } else {
+    let temporary = Math.round(ftocTemp * 10) / 10 + .1;
+    cTempRounded = Math.round(temporary * 10) / 10; 
+  }
+  console.log("1");
+  return cTempRounded;
 };
 
-const ctof = function() {
+const ctof = function(cTemp) {
+  let ctofTemp = (cTemp * 1.8) + 32;
+  let fTempRounded = Math.round(ctofTemp * 10) / 10;
 
+  console.log("2");
+  return fTempRounded;
 };
 
 module.exports = {
