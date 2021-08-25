fdd299d010b2ef5241c42e849c32cf87ff2d52a1
caleblawson143 <56615142+caleblawson143@users.noreply.github.com>
2021-08-24 23:13:12 -0500

    Add files via upload

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...requestedRemovals) {
+let newArray = [];
 
+    arr.forEach((arrayItem) => {
+        if (!requestedRemovals.includes(arrayItem)) newArray.push(arrayItem);
+    });
+
+    return newArray;
 };
 
 module.exports = removeFromArray;
